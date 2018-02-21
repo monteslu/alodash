89b2ff68b94fe011dc83da91c05cb016e52c42aa
Luis Montes <luis@iceddev.com>
2018-02-21 12:29:53 -0700

    cleanup test

--- test/index.js
+++ test/index.js
@@ -5,25 +5,25 @@ const chai = require('chai');
 const expect = chai.expect;
 chai.should();
 
-const prodash = require('../');
+const alodash = require('../');
 
-describe('prodash', function(){
+describe('alodash', function(){
 
   it('should destructure a function', function(done){
-    const { aget } = prodash;
+    const { aget } = alodash;
     aget.should.be.a('function');
     done();
   });
 
   it('should return lodash from a _', function(done){
-    const { _ } = prodash;
+    const { _ } = alodash;
     _.should.be.a('function');
     done();
   });
 
   it('should call a lodash function with a promise', function(done){
     const promise = Promise.resolve({some:{ nested: {prop: 1}}});
-    const { aget } = prodash;
+    const { aget } = alodash;
     aget(promise, 'some.nested.prop')
       .then(function(val) {
         val.should.equal(1);
